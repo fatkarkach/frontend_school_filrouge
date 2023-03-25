2f724a87a20c3981a36332542633e7bce38a47a8
fatkarkach <fatma.karkach@um5r.ac.ma>
2023-03-25 03:15:59 +0000

    version2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,16 @@ import {Component, HostListener} from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isNavbarScrolled = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.pageYOffset > 0) {
+      this.isNavbarScrolled = true;
+    } else {
+      this.isNavbarScrolled = false;
+    }
+  }
 
 
 }
